e70800a61821064af110986e278c15cc526ba613
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-07-26 11:44:14 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 246ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240726/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[N][N];
+        dp[0][0] = map[0][0];
+        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (isIn(i-1, j) && isIn(i, j-1)) {
+                    dp[i][j] = Math.max(Math.min(dp[i-1][j], map[i][j]), Math.min(dp[i][j-1], map[i][j]));
+                }
+                else if (isIn(i-1, j)) {
+                    dp[i][j] = Math.min(dp[i-1][j], map[i][j]);
+                }
+                else if (isIn(i, j-1)) {
+                    dp[i][j] = Math.min(dp[i][j-1], map[i][j]);
+                }
+            }
+        }
+
+        System.out.println(dp[N-1][N-1]);
+    }
+
+    static void printMap(int[][] map) {
+       for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        } 
+    }
+
+    static boolean isIn(int x, int y) {
+        return x>=0 && x<N && y>=0 && y<N;
+    }
+}
