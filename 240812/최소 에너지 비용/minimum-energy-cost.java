c8fb695ceb3e241db6223455b0c0dcc210c4bffb
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-08-12 22:41:18 +0900

    Title: 최소 에너지 비용, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240812/최소 에너지 비용/minimum-energy-cost.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        int[] dist = new int[N-1];
+        for (int i=0; i<N-1; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] cost = new int[N];
+        for (int i=0; i<N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] min_cost = new int[N];
+        min_cost[0] = cost[0];
+        // 현재까지 에너지를 채우는데 드는 최소 비용을 저장
+        for (int i=1; i<N; i++) {
+            min_cost[i] = Math.min(min_cost[i-1], cost[i]);
+        }
+
+        int ans = 0;
+        for (int i=0; i<N-1; i++) {
+            ans += dist[i] * min_cost[i];
+        }
+
+        System.out.println(ans);
+    }
+}
