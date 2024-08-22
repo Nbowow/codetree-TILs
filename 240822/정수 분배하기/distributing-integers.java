965c32d47fa2c892e1123ff51d76228cf527c47e
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-08-22 09:53:51 +0900

    Title: 정수 분배하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240822/정수 분배하기/distributing-integers.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] nums;
+
+    static boolean parametricSearch(int cur) {
+        
+        // cur숫자로 분배하여 m개 이상 얻을 수 있는지 확인
+        int totalCount = 0;
+        for (int i=0; i<N; i++) {
+            int q = nums[i] / cur;
+
+            totalCount += q;
+
+            if (totalCount >= M) return true;
+        }
+
+        return false;
+        
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        for (int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums[N-1];
+        int ans = 0;
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (parametricSearch(mid)) {
+                left = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+
+            else right = mid - 1;
+
+        }
+
+        System.out.println(ans);
+    }
+}
