190f052370b5e5428fb8b615aba9f05dabdbd0db
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-08-22 10:24:08 +0900

    Title: 삼 오 무, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240822/삼 오 무/three-five-moo.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static long N;
+
+    static long parameticSearch(long cur) {
+
+        long mooCount = cur / 3 + cur / 5 - cur / 15;
+
+        return cur - mooCount;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Long.parseLong(br.readLine());
+
+        long left = 0;
+        long right = (long) 1000000000;
+        long ans = 0;
+        while (left <= right) {
+
+            long mid = (left + right) / 2;
+
+            if (parameticSearch(mid) == N) {
+                ans = mid;
+                break;
+            }
+
+            else if (parameticSearch(mid) > N) {
+                right = mid - 1;
+            }
+
+            else left = mid + 1;
+        }
+
+        System.out.println(ans);
+    }
+}
